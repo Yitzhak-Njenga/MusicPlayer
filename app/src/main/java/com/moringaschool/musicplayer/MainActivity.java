954ac50606ab12c68013ae1cd2bac578c6b584b1
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-19 21:39:51 +0300

    added a new activity music activity

--- app/src/main/java/com/moringaschool/musicplayer/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicplayer/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(MainActivity.this,MusicActivity.class);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        };thread.start();
     }
 }
