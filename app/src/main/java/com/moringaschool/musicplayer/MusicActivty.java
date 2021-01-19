b8bde3df88f57f429026bb4ae7ffade8651449b1
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-19 22:09:05 +0300

    added a new activity

--- app/src/main/java/com/moringaschool/musicplayer/MusicActivty.java
+++ app/src/main/java/com/moringaschool/musicplayer/MusicActivty.java
@@ -2,7 +2,9 @@ package com.moringaschool.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import butterknife.BindView;
@@ -17,5 +19,13 @@ public class MusicActivty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_activty);
         ButterKnife.bind(this);
+
+        MusicLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MusicActivty.this)
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/moringaschool/musicplayer/MusicLibraryActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_library);
+    }
+}
