402b5536aff624a54b1a771eb61c27c9cec6117a
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-19 23:30:30 +0300

    reference the api key to build config

---
+++ app/src/main/java/com/moringaschool/musicplayer/Constants.java
@@ -0,0 +1,8 @@
+package com.moringaschool.musicplayer;
+
+public class Constants {
+    public static final String DEEZER_Api = BuildConfig.DEEZERAPI;
+    public static final String DEZER_API_BASE_URL= "https://api.deezer.com/";
+
+
+}
