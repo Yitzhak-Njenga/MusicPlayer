856eedd195b8e82496a3a7d0d6819e80e63d1f19
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-20 08:34:07 +0300

    created an adapter

---
+++ app/src/main/java/com/moringaschool/musicplayer/adapter/AlbumsAdapter.java
@@ -0,0 +1,4 @@
+package com.moringaschool.musicplayer.adapter;
+
+public class AlbumsAdapter {
+}
--- app/src/main/java/com/moringaschool/musicplayer/services/MuscClient.java
+++ app/src/main/java/com/moringaschool/musicplayer/services/MuscClient.java
@@ -1,4 +1,25 @@
 package com.moringaschool.musicplayer.services;
 
+import com.moringaschool.musicplayer.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MuscClient {
+
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = Constants.DEZER_API_BASE_URL;
+
+
+    public static Retrofit getClient(){
+        if (retrofit== null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit;
+    }
+
 }
