ea479243255bfcbe795c8d21d53011b21142ddd4
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-21 06:12:17 +0300

    added a style file

--- app/src/main/java/com/moringaschool/musicplayer/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.app_title) TextView mApp_tittle;
 
 
+   Animation DownTop,Fade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        DownTop = AnimationUtils.loadAnimation(this,R.anim.downtop);
+        Fade= AnimationUtils.loadAnimation(this,R.anim.fade);
+
+        mLinearLayout2.setAnimation(DownTop);
+        mApp_tittle.setAnimation(Fade);
 
         final Intent intent = new Intent(MainActivity.this,MusicActivty.class);
         Thread thread = new Thread(){
